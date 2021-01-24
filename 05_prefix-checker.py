896881e54571f515563dbd922014f40c0fffaac9
LS <ltestevens@gmail.com>
2021-01-24 21:49:30 +0000

    Fixes issue with the second string being longer than the first string.

--- 05_prefix-checker.py
+++ 05_prefix-checker.py
@@ -7,11 +7,12 @@ prefixVeri = False
 
 # Runs through the two strings for the length of the second and checks if each character is equall.
 # Then sets the prefixVeri accordingly
-for i in range(0,len(str2)):
-    if str1[i] == str2[i]:
-        prefixVeri = True
-    else:
-        break
+if len(str2) <= len(str1):
+    for i in range(0,len(str2)):
+        if str1[i] == str2[i]:
+            prefixVeri = True
+        else:
+            break
 
 # Prints the result of the algorithm
 print(prefixVeri)
