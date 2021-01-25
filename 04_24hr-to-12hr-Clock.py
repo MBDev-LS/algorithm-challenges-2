d1177042defa256eed62152430ad9496350bbd1b
LS <ltestevens@gmail.com>
2021-01-25 10:11:07 +0000

    Adds comments for the time converter.

--- 04_24hr-to-12hr-Clock.py
+++ 04_24hr-to-12hr-Clock.py
@@ -1,5 +1,7 @@
+# Takes 24 hour input from the user
 hr24Input = int(input('Enter the 24 hour number: '))
 
+# Determines the suffix and has catches for boundary cases.
 if hr24Input > 12:
     suffix = 'PM'
     time = hr24Input - 12
@@ -16,4 +18,6 @@ else:
     suffix = 'AM'
     time = hr24Input
 
+
+# Outputs the result.
 print(time, suffix)
